67fbd097d4793ba95ff6908df7458df97f9f3b80
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 16:25:20 +0100

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+"Comment(models.Model)"
 class Post(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
