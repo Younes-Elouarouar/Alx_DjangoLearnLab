8b5e6e3b85d4f0670e2c5c06a509aa03ee06d5db
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 16:26:00 +0100

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 "Comment(models.Model)"
+"viewsets", "viewsets.ModelViewSet", "Comment.objects.all()"
 class Post(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
 class Like(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
