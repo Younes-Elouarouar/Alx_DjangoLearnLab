976d698144df17da67a76f8828c6afecd873f87d
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 15:34:35 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
+from .views import FeedViewSet
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(r'feed', FeedViewSet, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
