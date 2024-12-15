b7721d851474c9dd1294122038fa2abc440408cf
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 14:54:59 +0100

    Create settings.py

---
+++ social_media_api/social_media_api/settings.py
@@ -0,0 +1,21 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',              # Django REST Framework
+    'rest_framework.authtoken',    # Token authentication support
+    'accounts',                    # Your custom accounts app
+]
+
+# Use custom user model
+AUTH_USER_MODEL = 'accounts.User'
+
+# Django REST Framework configuration (optional)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
