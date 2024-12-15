acd1167017892327ed1ebfe445af29b8b7c54c78
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 16:22:25 +0100

    Update models.py

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -4,4 +4,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 class Notification(models.Model):
-    recipient = models.ForeignKey(get_user_model(), related
+    recipient = models.ForeignKey(get_user_model(), related_name='notifications', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
