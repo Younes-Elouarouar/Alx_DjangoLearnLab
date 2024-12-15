ee3c6ddad7dc37a5c8083b49933e37418211b0d0
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 14:59:04 +0100

    Create serializers.py

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
+
+# Registration Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+# Login Serializer
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        return user
