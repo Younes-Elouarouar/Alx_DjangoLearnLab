f91fffcf496d04aa303f9b215e7192a8afbafe15
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 15:33:23 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,45 +1,19 @@
 from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
+from .models import Post
+from .serializers import PostSerializer
+from django.contrib.auth import get_user_model
 
-# Post ViewSet
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+User = get_user_model()
+
+class FeedViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        # Set the author to the currently authenticated user
-        serializer.save(author=self.request.user)
+    def list(self, request):
+        # Get posts from users the current user is following
+        followed_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
 
-    @action(detail=True, methods=['post'])
-    def like(self, request, pk=None):
-        post = self.get_object()
-        # Implement "like" logic if required (e.g., add to liked users list)
-        return Response({"status": "liked"})
-
-    def destroy(self, request, *args, **kwargs):
-        post = self.get_object()
-        if post.author != request.user:
-            raise PermissionDenied("You do not have permission to delete this post.")
-        return super().destroy(request, *args, **kwargs)
-
-# Comment ViewSet
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        # Set the author to the currently authenticated user
-        post = Post.objects.get(id=self.request.data['post'])
-        serializer.save(author=self.request.user, post=post)
-
-    def destroy(self, request, *args, **kwargs):
-        comment = self.get_object()
-        if comment.author != request.user:
-            raise PermissionDenied("You do not have permission to delete this comment.")
-        return super().destroy(request, *args, **kwargs)
+        # Serialize the posts
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
