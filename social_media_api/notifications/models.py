2e1008ff29ef49b1c5645fe4eead94c036093e24
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 16:21:56 +0100

    Create models.py

---
+++ social_media_api/notifications/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(get_user_model(), related
