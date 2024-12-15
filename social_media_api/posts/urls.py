e97a9f59011aeaef3d1a6ade66f703b1cf9fb9ad
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 15:59:53 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -6,6 +6,8 @@ router = DefaultRouter()
 router.register(r'feed', FeedView, basename='feed')
 
 urlpatterns = [
+    path('feed/', FeedView.as_view(), name='feed'),
+
     path('"feed/"', include(feeds.urls))
     path('', include(router.urls)),
 ]
