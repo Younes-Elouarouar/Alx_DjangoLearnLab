854cf1420b582a19ff0f2b03668ca72f26e809d3
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-15 16:23:38 +0100

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
+"CustomUser.objects.all()"
 # Follow User
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
