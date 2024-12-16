6f772c6dd004e94ab48b1d93791fe3d899e6b791
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-16 20:07:05 +0100

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,7 +1,7 @@
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+"bio", "profile_picture", "models.ImageField", "models.TextField"
 class CustomUser(AbstractUser):
     following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
 
